2fb0bd20caa58cc553bed6c6ee4f6f92598aac62
Kopec9 <kopecmar69@gmail.com>
2024-02-13 18:00:05 +0100

    Update NnpiaCv1Application.java
    Něco dalšího

--- src/main/java/upce/nnpia_cv_1/NnpiaCv1Application.java
+++ src/main/java/upce/nnpia_cv_1/NnpiaCv1Application.java
@@ -1,13 +1,18 @@
 package upce.nnpia_cv_1;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import upce.nnpia_cv_1.Controllers.MyController;
 
 @SpringBootApplication
 public class NnpiaCv1Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(NnpiaCv1Application.class, args);
+
+		ApplicationContext context = SpringApplication.run(NnpiaCv1Application.class, args);
+
+		MyController myController = context
+
 	}
 
 }
